7fd2ab0832265f5b16c62010f293a9ca77916cec
Emiliano Bernal <emiibernal1275@gmail.com>
2024-12-10 20:42:33 -0300

    Update

--- Practicos/codigosTeoria.hs
+++ Practicos/codigosTeoria.hs
@@ -9,3 +9,58 @@ mifact x y = if x == 0 then y else mifact (x-1) (x*y)
 factorial :: Int -> Int 
 factorial n = mifact n 1
 
+--Definicion del take
+
+mytake :: Int -> [a] -> [a]
+mytake 0 xs = []
+mytake n [] = []
+mytake n (x:xs) = x : mytake(n-1) xs 
+
+--Definicion de drop
+
+mydrop :: Int -> [a] -> [a]
+mydrop 0 xs = xs
+mydrop _ [] = []
+mydrop n (x:xs) = drop (n-1) xs
+
+--Definicion de suma para ver los patrones de recursion
+
+mysum :: [Int] -> Int 
+mysum [] = 0
+mysum (x:xs) = x + mysum xs
+
+--Definiciones de foldr y foldl
+
+myfoldr :: (a -> b -> b) -> b -> [a] -> b
+myfoldr f z [] = z  --Z es el elemento que devolvemos en caso de tener una lista vacia
+myfoldr f z (x:xs) = f x (myfoldr f z xs)
+
+myfoldl :: (a-> b -> a) -> a -> [b] -> a
+myfoldl f z [] = z
+myfoldl f z (x:xs) = myfoldl f (f z x) xs
+
+myfoldl' :: (a -> b -> a) -> a -> [b] -> a
+myfoldl' f z [] = z
+myfoldl' f z (x:xs) = let z' = z `f` x
+                      in seq z' (myfoldl' f z' xs)
+
+--Caso base: Si la lista esta vacia, devuelvo el neutro de la operacion
+--Caso recursivo: Cuando uso el let defino una "variable" z', en donde, aplicamos la funcion f al acumulador z y a al primer elemento de la lista. Para luego, en in, fuerzo la evaluacion del z' para luego ser llamado nuevamente recursivamente con el resto de la lista
+
+--Definicion de map
+
+mymap :: (a -> b) -> [a] -> [b]
+mymap f [] = []
+mymap f (x:xs) = f x : mymap f xs
+
+myfilter :: (a -> Bool) -> [a] -> [a]
+myfilter f [] = []
+myfilter f (x:xs)
+    | f x = x : myfilter f xs
+    | otherwise = myfilter f xs
+
+isEven :: Integral a => a -> Bool
+isEven a = if a `mod` 2 == 0 then True else False
+
+removeNonUppercase :: String -> String
+removeNonUppercase st = [c | c <- st, c `elem` ['A'..'Z']]
--- Practicos/practico5.hs
+++ Practicos/practico5.hs
@@ -1,5 +1,121 @@
 {-Practico Nro 5-}
 
 {-Actividad 1-}
+lisInf 1 = 1 : lisInf 1
 
+{-Actividad 2-}
 
+lisInf n = n : lisInf (n+1)
+
+{-Actividad 3-}
+
+primNumeros :: Int -> [Int]
+primNumeros n = [0..n-1]
+
+{-Actividad 4-}
+
+lisInf5 :: Int -> [Int]
+lisInf5 n = take 5 [n..]
+
+{-Actividad 5-}
+
+cuadrados :: [Int] -> [Int]
+cuadrados = map (^2)
+
+{-Actividad 6-}
+
+divisore :: Int -> [Int]
+divisore n = let x = [1..n]
+             in filter (\x -> n `mod` x == 0) x
+
+{-Actividad 7-}
+
+esPrimo :: Int -> Bool
+esPrimo n
+    | n < 2 = False
+    | otherwise = null [x | x <- [2..n-1], n `mod` x == 0]
+
+listaPrimos :: [Int] -> [Int]
+listaPrimos xs = filter esPrimo xs
+ 
+{-Actividad 8-}
+
+sumaCuadrados :: [Int] -> Int
+sumaCuadrados [] = 0
+sumaCuadrados xs = sum (map (^2) xs)
+
+{-Actividad 9-}
+
+listaSucesores :: [Int] -> [Int]
+listaSucesores xs = map (+1) xs
+
+{-Actividad 10-}
+
+sumaLista :: [Int] -> Int
+sumaLista xs = foldr (+) 0 xs
+
+{-Actividad 11-}
+
+factorial :: Int -> Int
+factorial 0 = 0
+factorial 1 = 0
+factorial n = foldr (*) 1 (list n)
+
+list :: Int -> [Int]
+list 0 = []
+list 1 = [1]
+list n = n : list (n-1)
+
+{-Actividad 12-}
+
+myand :: [Bool] -> Bool
+myand [] = False
+myand xs = foldr (&&) True xs
+
+{-Actividad 13-}
+
+tam :: [a] -> Int
+tam [] = 0
+tam [x] = 1
+tam (x:xs) = foldr (const (+ 1)) 0 xs
+
+-- const (+ 1) es una función que siempre devuelve (+ 1), independientemente de su argumento.
+
+taml :: [a] -> Int
+taml [] = 0
+taml [x] = 1
+taml (x:xs) = foldl (\acc _ -> acc + 1 ) 0 xs
+
+-- _ funciona como un placeholder para cada elemento de la lista, pero no le interesa cual es el elemento. Mientras haya elemento incrementa el acumulador
+-- El 0 funciona como el valor INICIAL del acumulador
+
+{-Actividad 14-}
+
+sucesoresLista :: [Int] -> [Int]
+sucesoresLista xs = [x+1 | x <- xs]
+
+{-Actividad 15-}
+
+cuadradosLista :: [Int] -> [Int]
+cuadradosLista xs = [x^2 | x <- xs]
+
+{-Actividad 16-}
+
+paresMayoresDiez :: [Int] -> [Int]
+paresMayoresDiez xs = [x | x <- xs, esPrimo x && x>10]
+
+{-Actividad 17-}
+
+divisoresLista :: Int -> [Int]
+divisoresLista n = [x | x <- [1..n], n `mod` x == 0]
+
+{-Actividad 18-}
+
+todosOcurrenEn :: (Eq a) => [a] -> [a] -> Bool
+todosOcurrenEn xs ys =  null [x| x <- xs,  x `notElem` ys] 
+
+--Null verifica si la lista esta vacia, si fuera ese el caso, devuelve true
+
+{-Actividad 19-}
+
+numerosPrimos :: Int -> [Int]
